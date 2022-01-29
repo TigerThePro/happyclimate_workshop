4a9c7cf96bfdaf8499375343c59a7a534b73cd28
Tiger <tigerthepro@gmail.com>
2022-01-29 02:56:23 -0800

    bug fixes

--- carbon_calc.js
+++ carbon_calc.js
@@ -14,12 +14,14 @@ co2.set("tv", 824);
 co2.set("jeans", 33);
 co2.set("shoes", 14);
 co2.set("tops", 10);
-co2.set("bottom", 9);
+co2.set("bottoms", 9);
 co2.set("dresses_suits", 16);
 // travel
 co2.set("driving", 21.008) // 0.404*52
 co2.set("carpool", 21.008) // 0.404*y*52*(x-1)
 // x*(y*28.834+102.06) for flight
+
+var final_co2 = new Map();
 
 function calc_carbon(x, y, category) {
   if (category == "flight") {
@@ -40,12 +42,15 @@ function calc_carbon(x, y, category) {
 // diet_yes
 document.getElementById("diet_yes").addEventListener("click", function(e) {
   var input = this.value;
-  document.getElementById("diet_ouput").innerHTML = calc_carbon(input, 1, "plant_diet");
+  var result = calc_carbon(input, 1, "plant_diet")
+  document.getElementById("diet_output").innerHTML = result;
+  final_co2.set("diet", result);
 })
 
 // diet_no
 document.getElementById("diet_no").addEventListener("click", function(e) {
-  document.getElementById("diet_ouput").innerHTML = 0
+  document.getElementById("diet_output").innerHTML = 0
+  final_co2.set("diet", 0);
 })
 
 
@@ -56,7 +61,9 @@ for (i = 0; i < normal_inputs.length; i++) {
     var x = this.value;
     var id = this.id;
     var output = id+"_"+"output";
-    document.getElementById(output).innerHTML = calc_carbon(x, 1, id);
+    var result = calc_carbon(x, 1, id);
+    document.getElementById(output).innerHTML = result;
+    final_co2.set(id, result);
   })
 }
 
@@ -64,26 +71,39 @@ for (i = 0; i < normal_inputs.length; i++) {
 document.getElementById("flight_x").addEventListener("input", function(e) {
   var x = this.value;
   var y = document.getElementById("flight_y").value;
-  document.getElementById("flight_output").innerHTML = calc_carbon(x, y, "flight");
+  var result = calc_carbon(x, y, "flight");
+  document.getElementById("flight_output").innerHTML = result;
+  final_co2.set("flight", result);
 })
 
 // flight y
 document.getElementById("flight_y").addEventListener("input", function(e) {
   var x = document.getElementById("flight_x").value;
   var y = this.value;
-  document.getElementById("flight_output").innerHTML = calc_carbon(x, y, "flight");
+  var result = calc_carbon(x, y, "flight");
+  document.getElementById("flight_output").innerHTML = result;
+  final_co2.set("flight", result);
 })
 
 // carpool x
 document.getElementById("carpool_x").addEventListener("input", function(e) {
   var x = this.value;
   var y = document.getElementById("carpool_y").value;
-  document.getElementById("carpool_output").innerHTML = calc_carbon(x, y, "carpool");
+  var result = calc_carbon(x, y, "carpool");
+  document.getElementById("carpool_output").innerHTML = result;
+  final_co2.set("carpool", result);
 })
 
 // carpool y
 document.getElementById("carpool_y").addEventListener("input", function(e) {
   var x = document.getElementById("carpool_x").value;
   var y = this.value;
-  document.getElementById("carpool_output").innerHTML = calc_carbon(x, y, "carpool");
+  var result = calc_carbon(x, y, "carpool");
+  document.getElementById("carpool_output").innerHTML = result;
+  inal_co2.set("carpool", result);
 })
+
+// submit
+// document.getElementById("submit").addEventListener("click", function(e) {
+//   document.write("")
+// })
