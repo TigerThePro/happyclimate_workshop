8f3cb5ccc0b3465952aab5cd461ab02762d31be8
Tiger <tigerthepro@gmail.com>
2022-01-28 23:26:05 -0800

    added carbon_calc.js, fixed a name in index.html

---
+++ carbon_calc.js
@@ -0,0 +1,22 @@
+var co2 = new Map();
+// food
+co2.set("plant_diet", 940);
+co2.set("red_meat_x", 237.12); // 4.56*52
+co2.set("other_meat_x", 36.4);   // 0.70*52
+co2.set("cheese_x", 14.04);  // 0.27*52
+co2.set("dairy_milk_x", 28.6);   // 0.55*52
+co2.set("chocolate_x", 48.88);  // 0.94*52
+co2.set("coffee_x", 8.84);   // 0.17*52
+// shopping
+co2.set("phone_x", 72);
+co2.set("laptop_x", 185);
+co2.set("tv_x", 824);
+co2.set("jeans_x", 33);
+co2.set("shoes_x", 14);
+co2.set("tops_x", 10);
+co2.set("bottom_x", 9);
+co2.set("dresses_suits_x", 16);
+// travel
+co2.set("driving_x", 21.008) // 0.404*52
+co2.set("carpool_xy", 21.008) // 0.404*52
+// x*(y*28.834+102.06)
