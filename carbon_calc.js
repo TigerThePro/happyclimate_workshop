0a5d4ac8b1c58833aa20ee46dab2cd7b30b898db
Tiger <tigerthepro@gmail.com>
2022-01-29 12:56:21 -0800

    added basic report feature

--- carbon_calc.js
+++ carbon_calc.js
@@ -21,8 +21,33 @@ co2.set("driving", 21.008) // 0.404*52
 co2.set("carpool", 21.008) // 0.404*y*52*(x-1)
 // x*(y*28.834+102.06) for flight
 
-var final_co2 = new Map();
+// clear localstorage
+function clear_storage() {
+  // food
+  localStorage.setItem("diet", 0);
+  localStorage.setItem("red_meat", 0);
+  localStorage.setItem("other_meat", 0);
+  localStorage.setItem("cheese", 0);
+  localStorage.setItem("dairy_milk", 0);
+  localStorage.setItem("chocolate", 0);
+  localStorage.setItem("coffee", 0);
+  // shopping
+  localStorage.setItem("phone", 0);
+  localStorage.setItem("laptop", 0);
+  localStorage.setItem("tv", 0);
+  localStorage.setItem("jeans", 0);
+  localStorage.setItem("shoes", 0);
+  localStorage.setItem("tops", 0);
+  localStorage.setItem("bottoms", 0);
+  localStorage.setItem("dresses_suits", 0);
+  // travel
+  localStorage.setItem("driving", 0);
+  localStorage.setItem("flight", 0);
+  localStorage.setItem("carpool", 0);
+}
 
+
+// helper function to calculate
 function calc_carbon(x, y, category) {
   if (category == "flight") {
     return x * ((y * 28.834) + 102.06);
@@ -39,19 +64,21 @@ function calc_carbon(x, y, category) {
   }
 }
 
-// diet_yes
-document.getElementById("diet_yes").addEventListener("click", function(e) {
-  var input = this.value;
-  var result = calc_carbon(input, 1, "plant_diet")
-  document.getElementById("diet_output").innerHTML = result;
-  final_co2.set("diet", result);
-})
+if (document.getElementById("diet_yes") && document.getElementById("diet_no")) {
+  // diet_yes
+  document.getElementById("diet_yes").addEventListener("click", function(e) {
+    var input = this.value;
+    var result = calc_carbon(input, 1, "plant_diet");
+    document.getElementById("diet_output").innerHTML = result;
+    localStorage.setItem("diet", result);
+  })
 
-// diet_no
-document.getElementById("diet_no").addEventListener("click", function(e) {
-  document.getElementById("diet_output").innerHTML = 0
-  final_co2.set("diet", 0);
-})
+  // diet_no
+  document.getElementById("diet_no").addEventListener("click", function(e) {
+    document.getElementById("diet_output").innerHTML = 0
+    localStorage.setItem("diet", 0);
+  })
+}
 
 
 // all normal class
@@ -63,47 +90,74 @@ for (i = 0; i < normal_inputs.length; i++) {
     var output = id+"_"+"output";
     var result = calc_carbon(x, 1, id);
     document.getElementById(output).innerHTML = result;
-    final_co2.set(id, result);
+    localStorage.setItem(id, result);
   })
 }
 
-// flight x
-document.getElementById("flight_x").addEventListener("input", function(e) {
-  var x = this.value;
-  var y = document.getElementById("flight_y").value;
-  var result = calc_carbon(x, y, "flight");
-  document.getElementById("flight_output").innerHTML = result;
-  final_co2.set("flight", result);
-})
+if (document.getElementById("flight_x") && document.getElementById("flight_y")) {
+  // flight x
+  document.getElementById("flight_x").addEventListener("input", function(e) {
+    var x = this.value;
+    var y = document.getElementById("flight_y").value;
+    var result = calc_carbon(x, y, "flight");
+    document.getElementById("flight_output").innerHTML = result;
+    localStorage.setItem("flight", result);
+  })
 
-// flight y
-document.getElementById("flight_y").addEventListener("input", function(e) {
-  var x = document.getElementById("flight_x").value;
-  var y = this.value;
-  var result = calc_carbon(x, y, "flight");
-  document.getElementById("flight_output").innerHTML = result;
-  final_co2.set("flight", result);
-})
+  // flight y
+  document.getElementById("flight_y").addEventListener("input", function(e) {
+    var x = document.getElementById("flight_x").value;
+    var y = this.value;
+    var result = calc_carbon(x, y, "flight");
+    document.getElementById("flight_output").innerHTML = result;
+    localStorage.setItem("flight", result);
+  })
+}
 
-// carpool x
-document.getElementById("carpool_x").addEventListener("input", function(e) {
-  var x = this.value;
-  var y = document.getElementById("carpool_y").value;
-  var result = calc_carbon(x, y, "carpool");
-  document.getElementById("carpool_output").innerHTML = result;
-  final_co2.set("carpool", result);
-})
 
-// carpool y
-document.getElementById("carpool_y").addEventListener("input", function(e) {
-  var x = document.getElementById("carpool_x").value;
-  var y = this.value;
-  var result = calc_carbon(x, y, "carpool");
-  document.getElementById("carpool_output").innerHTML = result;
-  inal_co2.set("carpool", result);
-})
+if (document.getElementById("carpool_x") && document.getElementById("carpool_y")) {
+  // carpool x
+  document.getElementById("carpool_x").addEventListener("input", function(e) {
+    var x = this.value;
+    var y = document.getElementById("carpool_y").value;
+    var result = calc_carbon(x, y, "carpool");
+    document.getElementById("carpool_output").innerHTML = result;
+    localStorage.setItem("carpool", result);
+  })
 
-// submit
-// document.getElementById("submit").addEventListener("click", function(e) {
-//   document.write("")
-// })
+  // carpool y
+  document.getElementById("carpool_y").addEventListener("input", function(e) {
+    var x = document.getElementById("carpool_x").value;
+    var y = this.value;
+    var result = calc_carbon(x, y, "carpool");
+    document.getElementById("carpool_output").innerHTML = result;
+    localStorage.setItem("carpool", result);
+  })
+}
+
+
+// run when report page loaded
+function make_report() {
+  report_food();
+}
+
+// report food
+function report_food() {
+
+    // var nm = new Map();
+    // nm.set("a", 0);
+    // nm.set("a", 2);
+    // console.log(nm.get("a"));
+
+  var food_total = 0;
+  food_total += Number(localStorage.getItem("diet"));
+  console.log(food_total);
+  food_total += Number(localStorage.getItem("red_meat"));
+  food_total += Number(localStorage.getItem("other_meat"));
+  food_total += Number(localStorage.getItem("cheese"));
+  food_total += Number(localStorage.getItem("dairy_milk"));
+  food_total += Number(localStorage.getItem("chocolate"));
+  food_total += Number(localStorage.getItem("coffee"));
+  
+  document.getElementById("food_report").innerHTML = food_total;
+}
