417689d61de7d28fd600c70e89eb9fba90c6547c
Tiger <tigerthepro@gmail.com>
2022-01-29 21:41:25 -0800

    php now collect info

--- submit.php
+++ submit.php
@@ -1,30 +1,24 @@
 <?php 
 
   if (isset($_POST["submit"])) {
-
     // link to post on how to handle multiple input at the same time
     // https://stackoverflow.com/questions/46016784/multiple-users-write-to-the-same-file-at-the-same-time-using-php#
-      // $filename="log.csv";
+      $filename="log.csv";
 
-      // $ip = $_SERVER["REMOTE_ADDR"];
-      // $beef = $_POST["beef"];
-      // $chicken = $_POST["chicken"];
-      // $milk = $_POST["milk"];
-      // $egg = $_POST["egg"];
+      $ip = $_SERVER["REMOTE_ADDR"];
 
-      // $myfile=fopen($filename, "a");
-      // fwrite($myfile, $ip);
-      // fwrite($myfile, ",");
-      // fwrite($myfile, $beef);
-      // fwrite($myfile, ",");
-      // fwrite($myfile, $chicken);
-      // fwrite($myfile, ",");
-      // fwrite($myfile, $milk);
-      // fwrite($myfile, ",");
-      // fwrite($myfile, $egg);
-      // fwrite($myfile, "\n");
-      // fclose($myfile);
-
+      $myfile=fopen($filename, "a");
+      // fwrite($myfile, $inputs); 
+      foreach ($_POST as $key => $value) {
+        fwrite($myfile, $value);
+        fwrite($myfile, ",");
+      }
+      fwrite($myfile, $ip);
+      fwrite($myfile, "\n");
+      
+      fclose($myfile);
+      
+      header("Location: report.html");
     exit();
   }
 ?>
